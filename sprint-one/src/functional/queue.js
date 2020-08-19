ad0c2518fa799555ca57cedb059082a4d522f658
Katie Poa <kpoa@berkeley.edu>
2020-08-19 15:30:55 -0700

    Implement queue (functional) passes all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,35 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    var top = storage['0'];
+    console.log('first item in queue', storage['0']);
+    console.log('fist item variable', top);
+    console.log('count', count);
+    console.log(storage);
+    delete storage['0'];
+    if (count > 0) {
+      count --;
+    }
+    for (var key in storage) {
+      var newKey = parseInt(key) - 1;
+      storage[newKey] = storage[key];
+      delete storage[key];
+    }
+    console.log('after shifting', storage);
+    return top;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
