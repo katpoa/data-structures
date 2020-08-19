be1fbb2e229b8d7950552f257882048cdf98f565
Katie Poa <kpoa@berkeley.edu>
2020-08-19 12:42:55 -0700

    Implement stack (functional) passes all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    var last = storage[count - 1];
+    delete storage[count - 1];
+    if (count > 0) {
+      count --;
+    }
+    return last;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
