7b0ec55ecc797d47580e211f2cd948cebf00006f
Katie Poa <kpoa@berkeley.edu>
2020-08-19 21:21:28 -0700

    Implement queue (prototypal) passes all tests!

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.storage = {};
+  queue.count = 0;
+
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  dequeue: function() {
+    var last = this.storage['0'];
+    delete last;
+    if (this.count > 0) {
+      this.count--;
+    }
+    for (var key in this.storage) {
+      var newKey = parseInt(key) - 1;
+      this.storage[newKey] = this.storage[key];
+      delete this.storage[key];
+    }
+    return last;
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
