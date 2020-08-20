f69a178584d8a5e3624b4cc238c2582c9fa7bdba
Katie Poa <kpoa@berkeley.edu>
2020-08-19 22:34:17 -0700

    Implement queue (ES6) passes all tests!

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,28 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+  dequeue() {
+    var last = this.storage['0'];
+    delete this.storage['0'];
+    if (this.count > 0) {
+      this.count--;
+    }
+    for (var key in this.storage) {
+      var newKey = parseInt(key) - 1;
+      this.storage[newKey] = this.storage[key];
+      delete this.storage[key];
+    }
+    return last;
+  }
+  size() {
+    return this.count;
   }
 
 }
