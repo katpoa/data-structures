c18a0959cda0a3e59d4c806cac8d588528033cb3
Victoria Chen <vicchen12@gmail.com>
2020-08-20 11:29:10 -0700

    attempt addToTail and  RemoveHead for linked lint

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (list.head === null) {
+      list.head = node;
+      node.next = list.tail;
+    } else {
+      var current = list.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = node;
+      console.log(current.next);
+      list.tail = current.next;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head.next === null) {
+      var temp = list.head;
+      list.head = null;
+      return temp;
+    }
+    var next = list.head.next;
+    var temp1 = list.head;
+    delete list.head;
+    list.head = next;
+    return temp1;
   };
 
   list.contains = function(target) {
