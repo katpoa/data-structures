19e6c040cebc72a685f2220b1f631e4c7a412dfe
Victoria Chen <vicchen12@gmail.com>
2020-08-20 22:09:34 -0700

    Implement tree (.addChild working, .contains very nearly working)

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,8 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,13 +10,58 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var node = {'value': value, 'children': []};
+  _.extend(node, treeMethods);
+  this.children.push(node);
+  console.log(this);
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, wasFound = false) {
+  if (this.value === target) {
+    return true;
+  }
+  //iterate newTree.children
+  for (var child = 0; child < this.children.length; child++) {
+    console.log('iterating through childen', this.children[child]);
+    if (this.children[child].value === target) {
+      wasFound = true;
+      return wasFound;
+    }
+    var tree2 = this.children[child];
+    //  else recursive call on child
+    if (tree2.children.length !== 0) {
+      tree2.contains(target, wasFound);
+    }
+  }
+  return wasFound;
 };
 
+treeMethods.size = function() {
+  var count = 0;
+  // basically contains but counting values
+  return count;
+};
 
+/*
+if (this.value === target) {
+  return true;
+}
+//iterate newTree.children
+for (var child = 0; child < this.children.length; child++) {
+  console.log('iterating through childen', this.children[child]);
+  if (this.children[child].value === target) {
+    return true;
+  }
+  var tree2 = this.children[child];
+  //  else recursive call on child
+  tree2.contains(target);
+}
+return false;
+}; */
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  addChild: constant O(1) - since we're only adding to the end
+  contains: linear O(n)
+
  */
