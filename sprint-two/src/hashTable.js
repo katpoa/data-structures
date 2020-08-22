d2a4bceb50d4652c577c40c1a3408356cd63c9ae
Victoria Chen <vicchen12@gmail.com>
2020-08-21 23:54:26 -0700

    Attempt insert method in HashTables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,13 +7,28 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  // check if bucket for index exists?
+  if (this._storage[index] !== undefined) {
+    this._storage[index].push([k, v]);
+  } else {
+    // if bucket doesnt already exist for index
+    var bucket = [];
+    bucket.push([k, v]);
+    this._storage.set(index, bucket);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //if bucket length is > 1
+  //  iterate through bucket array
+  //
+  this._storage.get(index); // should get index for v; })
 };
 
 HashTable.prototype.remove = function(k) {
+
   var index = getIndexBelowMaxForKey(k, this._limit);
 };
 
