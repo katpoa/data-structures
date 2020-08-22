625f2729acf47716e885593702c273e5c903c39a
Victoria Chen <vicchen12@gmail.com>
2020-08-22 16:11:55 -0700

    implement add, contains, and remove methods for sets (all tests pass)

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  var property = item.split(' ');
+  if (!(property[0] in this._storage)) {
+    this._storage[property[0]] = property[1];
+  }
 };
 
 setPrototype.contains = function(item) {
+  var property = item.split(' ');
+  return (property[0] in this._storage);
 };
 
 setPrototype.remove = function(item) {
+  var property = item.split(' ');
+  delete this._storage[property[0]];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  add: constant O(1) - worst case linear O(n)
+  contains: constant O(1) - worst case linear O(n)
+  remove: constant O(1)
  */
