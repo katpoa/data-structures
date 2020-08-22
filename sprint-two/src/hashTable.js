b844e5499e36acee82ffe48f47b055dcf098be07
Victoria Chen <vicchen12@gmail.com>
2020-08-22 12:36:38 -0700

    implement insert, retrieve, and remove for Hash tables with time complexity - passes all tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,10 +7,16 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
+  var targetBucket = this._storage.get(index);
   // check if bucket for index exists?
-  if (this._storage[index] !== undefined) {
-    this._storage[index].push([k, v]);
+  if ( targetBucket !== undefined) {
+    for (var i = 0; i < targetBucket.length; i++) {
+      if (targetBucket[i][0] === k) {
+        targetBucket[i][1] = v;
+      } else {
+        targetBucket.push([k, v]);
+      }
+    }
   } else {
     // if bucket doesnt already exist for index
     var bucket = [];
@@ -21,21 +27,29 @@ HashTable.prototype.insert = function(k, v) {
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //if bucket length is > 1
-  //  iterate through bucket array
-  //
-  this._storage.get(index); // should get index for v; })
+  var targetBucket = this._storage.get(index);
+  if (targetBucket === undefined) {
+    return undefined;
+  }
+  for (var i = 0; i < targetBucket.length; i++) {
+    if (targetBucket[i][0] === k) {
+      return targetBucket[i][1];
+    }
+  }
 };
+// if we were to accomodate collisions would potentially create more unique index based on both k + v rather than overwriting and each bucket carries unique
 
 HashTable.prototype.remove = function(k) {
-
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(index).pop();
 };
-
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  insert: constant O(1) (worst case: all tuples in same bucket array = linear  O(n))
+  retrieve: constant O(1) (worst case: all tuples in same bucket array = linear  O(n))
+  remove: constant O(1)
  */
 
 
